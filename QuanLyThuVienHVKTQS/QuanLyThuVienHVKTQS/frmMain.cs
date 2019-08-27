558f1064cb9747a6f65190ab43a65836bf125d54
Khoabdattt52 <phambangbang1235@gmail.com>
2019-08-27 08:19:24 +0700

    tạo UserController.cs

--- QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/frmMain.cs
+++ QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/frmMain.cs
@@ -16,5 +16,15 @@ namespace QuanLyThuVienHVKTQS
         {
             InitializeComponent();
         }
+
+        private void dangnhapbtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dangkybtn_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
