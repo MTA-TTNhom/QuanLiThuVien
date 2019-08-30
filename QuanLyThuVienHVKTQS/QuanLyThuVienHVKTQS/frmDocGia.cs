bb0039314430ed0a75798c1f371047c8aba55477
BuiThanhHoang <54828059+BuiThanhHoang@users.noreply.github.com>
2019-08-31 01:44:57 +0700

    update docgia

--- QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/frmDocGia.cs
+++ QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/frmDocGia.cs
@@ -22,6 +22,38 @@ namespace QuanLyThuVienHVKTQS
             InitializeComponent();
             HienThi_DG();
         }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void HienThi_DG()
+        {
+            var dg = new DocGiaController();
+            l = dg.Detail();
+            listView_DG.Items.Clear();
+            int i = 1;
+
+            sothetxt.Text = l[0].sothe.ToString();
+            tendgtxt.Text = l[0].hoten.ToString();
+            ngaysinhdg.Text = l[0].ngaysinh.ToString();
+            if (l[0].gioitinh == null) gioitinhdgtxt.Text = "";
+            else gioitinhdgtxt.Text = l[0].gioitinh.ToString();
+            if (l[0].email == null) emaildgtxt.Text = "";
+            else emaildgtxt.Text = l[0].email.ToString();
+            if (l[0].diachi == null) diachidgtxt.Text = "";
+            else diachidgtxt.Text = l[0].diachi.ToString();
+            if (l[0].socmtnd == null) socmtnddgtxt.Text = "";
+            else socmtnddgtxt.Text = l[0].socmtnd.ToString();
+            if (l[0].ngaylamthe == null) ngaythedg.Text = "";
+            else ngaythedg.Text = l[0].ngaylamthe.ToString();
+            if (l[0].handungthe == null) hanthedg.Text = "";
+            else hanthedg.Text = l[0].handungthe.ToString();
+        }
 
     }
 }
