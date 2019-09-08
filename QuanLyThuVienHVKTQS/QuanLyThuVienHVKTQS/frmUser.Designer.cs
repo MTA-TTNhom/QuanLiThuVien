a2c043a754f2a8a8ed4c78557d902c4596357665
Lê Ngọc Dự <55566554+hoten1701@users.noreply.github.com>
2019-09-08 21:00:30 +0700

    update fUser

---
+++ QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/frmUser.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace QuanLyThuVienHVKTQS
+{
+    partial class frmUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.quanLiHêThôngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.đăngXuâtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.thoatChươngTrinhToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quanLiDanhMucToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.danhMucĐôcGiaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.danhMucNhânViênToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.danhMucNhaXuâtBanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quanLiSachToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.quảnLýMượntrảSáchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView_User = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupuser = new System.Windows.Forms.GroupBox();
+            this.IDtxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.isadmincbx = new System.Windows.Forms.ComboBox();
+            this.passwordtxt = new System.Windows.Forms.TextBox();
+            this.usernametxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.thoat_userbtn = new System.Windows.Forms.Button();
+            this.boqua_userbtn = new System.Windows.Forms.Button();
+            this.luu_userbtn = new System.Windows.Forms.Button();
+            this.xoa_userbtn = new System.Windows.Forms.Button();
+            this.Sua_userbtn = new System.Windows.Forms.Button();
+            this.Them_userbtn = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.search_usertxt = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.groupuser.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+        }
+
+        #endregion
+    }
+}
