4c37b1f0662af58079d5f44c0b5cea3ee1e32c31
Lê Ngọc Dự <55566554+hoten1701@users.noreply.github.com>
2019-09-08 21:01:28 +0700

    Update fUser

--- QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/frmUser.Designer.cs
+++ QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/frmUser.Designer.cs
@@ -73,6 +73,176 @@
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quanLiHêThôngToolStripMenuItem,
+            this.quanLiDanhMucToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(584, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // quanLiHêThôngToolStripMenuItem
+            // 
+            this.quanLiHêThôngToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.đăngXuâtToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.thoatChươngTrinhToolStripMenuItem});
+            this.quanLiHêThôngToolStripMenuItem.Name = "quanLiHêThôngToolStripMenuItem";
+            this.quanLiHêThôngToolStripMenuItem.Size = new System.Drawing.Size(108, 20);
+            this.quanLiHêThôngToolStripMenuItem.Text = "Quản lí hệ thống";
+            // 
+            // đăngXuâtToolStripMenuItem
+            // 
+            this.đăngXuâtToolStripMenuItem.Name = "đăngXuâtToolStripMenuItem";
+            this.đăngXuâtToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.đăngXuâtToolStripMenuItem.Text = "Đăng xuất";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(174, 6);
+            // 
+            // thoatChươngTrinhToolStripMenuItem
+            // 
+            this.thoatChươngTrinhToolStripMenuItem.Name = "thoatChươngTrinhToolStripMenuItem";
+            this.thoatChươngTrinhToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.thoatChươngTrinhToolStripMenuItem.Text = "Thoát chương trình";
+            // 
+            // quanLiDanhMucToolStripMenuItem
+            // 
+            this.quanLiDanhMucToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.danhMucĐôcGiaToolStripMenuItem,
+            this.danhMucNhânViênToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.danhMucNhaXuâtBanToolStripMenuItem,
+            this.quanLiSachToolStripMenuItem,
+            this.toolStripSeparator3,
+            this.quảnLýMượntrảSáchToolStripMenuItem,
+            this.toolStripSeparator4,
+            this.toolStripMenuItem2});
+            this.quanLiDanhMucToolStripMenuItem.Name = "quanLiDanhMucToolStripMenuItem";
+            this.quanLiDanhMucToolStripMenuItem.Size = new System.Drawing.Size(114, 20);
+            this.quanLiDanhMucToolStripMenuItem.Text = "Quản lí danh mục";
+            // 
+            // danhMucĐôcGiaToolStripMenuItem
+            // 
+            this.danhMucĐôcGiaToolStripMenuItem.Name = "danhMucĐôcGiaToolStripMenuItem";
+            this.danhMucĐôcGiaToolStripMenuItem.Size = new System.Drawing.Size(200, 22);
+            this.danhMucĐôcGiaToolStripMenuItem.Text = "Danh mục độc giả";
+            // 
+            // danhMucNhânViênToolStripMenuItem
+            // 
+            this.danhMucNhânViênToolStripMenuItem.Name = "danhMucNhânViênToolStripMenuItem";
+            this.danhMucNhânViênToolStripMenuItem.Size = new System.Drawing.Size(200, 22);
+            this.danhMucNhânViênToolStripMenuItem.Text = "Danh mục nhân viên";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(197, 6);
+            // 
+            // danhMucNhaXuâtBanToolStripMenuItem
+            // 
+            this.danhMucNhaXuâtBanToolStripMenuItem.Name = "danhMucNhaXuâtBanToolStripMenuItem";
+            this.danhMucNhaXuâtBanToolStripMenuItem.Size = new System.Drawing.Size(200, 22);
+            this.danhMucNhaXuâtBanToolStripMenuItem.Text = "Danh mục nhà xuất bản";
+            // 
+            // quanLiSachToolStripMenuItem
+            // 
+            this.quanLiSachToolStripMenuItem.Name = "quanLiSachToolStripMenuItem";
+            this.quanLiSachToolStripMenuItem.Size = new System.Drawing.Size(200, 22);
+            this.quanLiSachToolStripMenuItem.Text = "Danh mục sách";
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(197, 6);
+            // 
+            // quảnLýMượntrảSáchToolStripMenuItem
+            // 
+            this.quảnLýMượntrảSáchToolStripMenuItem.Name = "quảnLýMượntrảSáchToolStripMenuItem";
+            this.quảnLýMượntrảSáchToolStripMenuItem.Size = new System.Drawing.Size(200, 22);
+            this.quảnLýMượntrảSáchToolStripMenuItem.Text = "Quản lý mượn,trả sách";
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(197, 6);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(200, 22);
+            this.toolStripMenuItem2.Text = "Quản lý người dùng";
+            // 
+            // listView_User
+            // 
+            this.listView_User.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView_User.FullRowSelect = true;
+            this.listView_User.GridLines = true;
+            this.listView_User.Location = new System.Drawing.Point(12, 27);
+            this.listView_User.Name = "listView_User";
+            this.listView_User.Size = new System.Drawing.Size(267, 267);
+            this.listView_User.TabIndex = 2;
+            this.listView_User.UseCompatibleStateImageBehavior = false;
+            this.listView_User.View = System.Windows.Forms.View.Details;
+            this.listView_User.SelectedIndexChanged += new System.EventHandler(this.ListView_User_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "STT";
+            this.columnHeader1.Width = 33;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "ID";
+            this.columnHeader2.Width = 39;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "UserName";
+            this.columnHeader3.Width = 73;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Password";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "IsAdmin";
+            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // groupuser
+            // 
+            this.groupuser.Controls.Add(this.IDtxt);
+            this.groupuser.Controls.Add(this.label4);
+            this.groupuser.Controls.Add(this.isadmincbx);
+            this.groupuser.Controls.Add(this.passwordtxt);
+            this.groupuser.Controls.Add(this.usernametxt);
+            this.groupuser.Controls.Add(this.label3);
+            this.groupuser.Controls.Add(this.label2);
+            this.groupuser.Controls.Add(this.label1);
+            this.groupuser.Location = new System.Drawing.Point(285, 75);
+            this.groupuser.Name = "groupuser";
+            this.groupuser.Size = new System.Drawing.Size(294, 158);
+            this.groupuser.TabIndex = 3;
+            this.groupuser.TabStop = false;
+            // 
+            // IDtxt
+            // 
+            this.IDtxt.Location = new System.Drawing.Point(98, 19);
+            this.IDtxt.Name = "IDtxt";
+            this.IDtxt.Size = new System.Drawing.Size(117, 20);
+            this.IDtxt.TabIndex = 7;
         }
 
         #endregion
