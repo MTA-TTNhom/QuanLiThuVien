482339450d3435bf9f19c4516c8dad6c302d0f23
BuiThanhHoang <54828059+BuiThanhHoang@users.noreply.github.com>
2019-09-09 09:09:56 +0700

    update code main

--- QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/frmMain.cs
+++ QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/frmMain.cs
@@ -15,14 +15,72 @@ namespace QuanLyThuVienHVKTQS
         public frmMain()
         {
             InitializeComponent();
+            Load_Main();
         }
-
-        private void dangnhapbtn_Click(object sender, EventArgs e)
+        public void Load_Main()
         {
+            quanLiDanhMucToolStripMenuItem.Enabled = false;
+            if (PhanQuyen.quyen == 1)
+            {
+                quanLiDanhMucToolStripMenuItem.Enabled = true;
+                đăngXuâtToolStripMenuItem.Enabled = true;
+                danhMucĐôcGiaToolStripMenuItem.Enabled = true;
+                danhMucNhânViênToolStripMenuItem.Enabled = true;
+                danhMucNhaXuâtBanToolStripMenuItem.Enabled = true;
+                quanLiSachToolStripMenuItem.Enabled = true;
+                quảnLýMượntrảSáchToolStripMenuItem.Enabled = true;
+                quanlynguoidung_toolStripMenuItem.Enabled = true;
 
+            }
+            else if (PhanQuyen.quyen == 0)
+            {
+                quanLiDanhMucToolStripMenuItem.Enabled = true;
+                đăngXuâtToolStripMenuItem.Enabled = true;
+                danhMucĐôcGiaToolStripMenuItem.Enabled = true;
+                danhMucNhânViênToolStripMenuItem.Enabled = true;
+                danhMucNhaXuâtBanToolStripMenuItem.Enabled = true;
+                quanLiSachToolStripMenuItem.Enabled = true;
+                quảnLýMượntrảSáchToolStripMenuItem.Enabled = true;
+                quanlynguoidung_toolStripMenuItem.Enabled = false;
+            }
+            else
+            {
+                đăngXuâtToolStripMenuItem.Enabled = false;
+                quanLiDanhMucToolStripMenuItem.Enabled = false;
+            }
         }
 
-        private void dangkybtn_Click(object sender, EventArgs e)
+        private void Show_NXB()
+        {
+            frmNhaXuatBan nxb = new frmNhaXuatBan();
+            nxb.ShowDialog();
+        }
+        private void Show_Sach()
+        {
+            frmSach s = new frmSach();
+            s.ShowDialog();
+        }
+        private void ShowDG()
+        {
+            frmDocGia dg = new frmDocGia();
+            dg.ShowDialog();
+        }
+        private void Show_NV()
+        {
+            frmNhanVien nv = new frmNhanVien();
+            nv.ShowDialog();
+        }
+        private void Show_MS()
+        {
+            frmTTMuonSach ms = new frmTTMuonSach();
+            ms.ShowDialog();
+        }
+        private void Show_User()
+        {
+            frmUser user = new frmUser();
+            user.ShowDialog();
+        }
+        private void toolStripButton1_Click(object sender, EventArgs e)
         {
 
         }
