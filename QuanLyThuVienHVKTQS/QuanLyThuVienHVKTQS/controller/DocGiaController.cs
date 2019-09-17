0b7a490c00603ab7d1bf81a4f5d8044d691152fe
HuynhLeHoang <chunickdeptrailam@gmail.com>
2019-09-17 23:04:48 +0700

    Huynh

--- QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/controller/DocGiaController.cs
+++ QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/controller/DocGiaController.cs
@@ -7,59 +7,44 @@ using System.Threading.Tasks;
 
 namespace QuanLyThuVienHVKTQS.controller
 {
-
     class DocGiaController
     {
-
         QuanLiThuVienHVKTQSDataContext db = null;
         QuanLiThuVienHVKTQSDataContext db1 = null;
-
         public DocGiaController()
         {
-
             db = new QuanLiThuVienHVKTQSDataContext();
             db1 = new QuanLiThuVienHVKTQSDataContext();
         }
 
         public List<docgia> Detail()
         {
-
             var list = db.docgias.ToList();
-
             return list;
         }
 
         public int Add(docgia dg)
         {
-
-
             try
             {
                 db.docgias.InsertOnSubmit(dg);
                 db.SubmitChanges();
                 return dg.sothe;
             }
-            catch (Exception)
+            catch(Exception)
             {
-
                 return 0;
                 throw;
-
             }
-
-
         }
-
 
         public bool Edit(docgia dg)
         {
-
-            var obj = db.docgias.First(m => m.sothe == dg.sothe);
-            if (obj != null)
+            var obj = db.docgias.First(m => m.sothe == dg.sothe );
+            if(obj!=null)
             {
                 try
                 {
-
                     obj.hoten = dg.hoten;
                     obj.ngaysinh = dg.ngaysinh;
                     obj.gioitinh = dg.gioitinh;
@@ -69,20 +54,14 @@ namespace QuanLyThuVienHVKTQS.controller
                     obj.ngaylamthe = dg.ngaylamthe;
                     obj.handungthe = dg.handungthe;
                     db.SubmitChanges();
-
                 }
                 catch (Exception)
                 {
-
                     return false;
                     throw;
-
                 }
-
             }
-
             return true;
-
         }
 
 
@@ -90,26 +69,16 @@ namespace QuanLyThuVienHVKTQS.controller
         {
             try
             {
-
                 var obj = db.docgias.First(m => m.sothe == id);
                 db.docgias.DeleteOnSubmit(obj);
                 db.SubmitChanges();
-
             }
-            catch (Exception)
+            catch(Exception)
             {
-
                 return false;
                 throw;
-
             }
-
-
             return true;
-
         }
-
     }
-
-
 }
--- QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/controller/MuonSachController.cs
+++ QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/controller/MuonSachController.cs
@@ -5,63 +5,43 @@ using System.Text;
 using System.Threading.Tasks;
 using QuanLyThuVienHVKTQS.Model;
 
-
 namespace QuanLyThuVienHVKTQS.controller
 {
     class MuonSachController
     {
-
-
         QuanLiThuVienHVKTQSDataContext db = null;
         public MuonSachController()
         {
-
             db = new QuanLiThuVienHVKTQSDataContext();
-
         }
 
         public List<MuonSach> Detail()
         {
-
             var list = db.MuonSaches.ToList();
             return list;
-
         }
-
-
 
         public int Add(MuonSach ms)
         {
-
             try
             {
                 db.MuonSaches.InsertOnSubmit(ms);
                 db.SubmitChanges();
                 return ms.Mamuon;
-
             }
-            catch (Exception)
+            catch(Exception)
             {
-
                 return 0;
                 throw;
-
             }
-
-
         }
-
-
-
         public bool Edit(MuonSach ms)
         {
             var obj = db.MuonSaches.First(m => m.Mamuon == ms.Mamuon);
-            if (obj != null)
+            if(obj!=null)
             {
-
                 try
                 {
-
                     obj.sothe = ms.sothe;
                     obj.masach = ms.masach;
                     obj.soluong = ms.soluong;
@@ -69,56 +49,35 @@ namespace QuanLyThuVienHVKTQS.controller
                     obj.hantra = ms.hantra;
                     obj.manv = ms.manv;
 
-
-
                     db.SubmitChanges();
                     return true;
                 }
-                catch (Exception)
+                catch(Exception)
                 {
-
                     return false;
                     throw;
-
                 }
-
             }
-
             else
             {
-
                 return false;
-
             }
-
-
         }
 
         public bool Delete(int id)
         {
             try
             {
-
                 var obj = db.MuonSaches.First(m => m.Mamuon == id);
                 db.MuonSaches.DeleteOnSubmit(obj);
-
                 db.SubmitChanges();
                 return true;
-
             }
-
-            catch (Exception)
+            catch(Exception)
             {
-
                 return false;
                 throw;
-
             }
-
         }
-
-
     }
-
-
 }
