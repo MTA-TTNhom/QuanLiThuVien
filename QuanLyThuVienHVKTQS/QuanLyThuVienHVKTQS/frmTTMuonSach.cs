d09cd2699a3a7fcc9a2957bf0b978b5e6ad5805a
Khoabdattt52 <phambangbang1235@gmail.com>
2019-09-18 07:23:38 +0700

    update FrmMuonSach.cs

--- QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/frmTTMuonSach.cs
+++ QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/frmTTMuonSach.cs
@@ -12,9 +12,68 @@ namespace QuanLyThuVienHVKTQS
 {
     public partial class frmTTMuonSach : Form
     {
+        List<sach> l = null;
+        private bool Them_bool = false;
+        private bool Sua_bool = false;
         public frmTTMuonSach()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void HienThiSach()
+        {
+            var s = new SachController();
+            l = s.Detail();
+            lvSach.Items.Clear();
+
+            var i = 1;
+
+            var nxb = new NhaXuatBanController();
+            List<nhaxuatban> listNXB = nxb.Detail();
+
+            cbbNXB.DataSource = listNXB;
+            cbbNXB.DisplayMember = "tennxb";
+            cbbNXB.ValueMember = "manxb".ToString();
+
+            txtMaSach.Text = l[0].masach.ToString();
+            txtTenSach.Text = l[0].tensach.ToString();
+            txtTacGia.Text = l[0].tentacgia.ToString();
+            if (l[0].nhaxuatban != null)
+                cbbNXB.Text = l[0].nhaxuatban.tennxb.ToString();
+            else
+                cbbNXB.Text = "";
+            txtNamXB.Text = l[0].namxb.ToString();
+            txtSoTrang.Text = l[0].sotrang.ToString();
+            txtGiaTien.Text = l[0].giatien.ToString();
+            txtSoLuong.Text = l[0].soluong.ToString();
+            txtNgonNgu.Text = l[0].ngonngu.ToString();
+            txtTheLoai.Text = l[0].theloai.ToString();
+            foreach (sach row in l)
+            {
+                ListViewItem item = new ListViewItem();
+                item.Text = "" + i++;
+
+                item.SubItems.Add(row.masach.ToString());
+                item.SubItems.Add(row.tensach.ToString());
+                item.SubItems.Add(row.theloai.ToString());
+                item.SubItems.Add(row.ngonngu.ToString());
+                item.SubItems.Add(row.soluong.ToString());
+
+                lvSach.Items.Add(item);
+            }
+            if (PhanQuyen.quyen == 1)
+                btn_edit(false);
+            else
+            {
+                grpEdit.Enabled = false;
+                btnThemSach.Enabled = btnSuaSach.Enabled = btnXoaSach.Enabled = btnLuuSach.Enabled = btnBoQua.Enabled = false;
+                btnThoat.Enabled = true;
+            }
         }
     }
 }
