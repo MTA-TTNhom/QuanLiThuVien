c428afa6bea1d764c6dca16f6ee4b16be498102f
asdcxsd <nguyenmanhthien98@gmail.com>
2019-09-22 18:39:39 +0700

    Update frmDocGia.cs

--- QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/frmDocGia.cs
+++ QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/frmDocGia.cs
@@ -256,7 +256,21 @@ namespace QuanLyThuVienHVKTQS
 
         private void label2_Click(object sender, EventArgs e)
         {
+            QuanLiThuVienHVKTQSDataContext db = new QuanLiThuVienHVKTQSDataContext();
+            int i = 1;
+            var lst = (from s in db.docgias where (s.sothe.ToString().Contains(searchtxt.Text) || s.hoten.ToString().Contains(searchtxt.Text)) select s).ToList();
+            listView_DG.Items.Clear();
+            foreach (docgia d in lst)
+            {
+                ListViewItem item = new ListViewItem();
+                item.Text = "" + i++;
+                item.SubItems.Add(d.sothe.ToString());
+                item.SubItems.Add(d.hoten.ToString());
+                item.SubItems.Add(d.ngaysinh.ToString());
+                item.SubItems.Add(d.ngaylamthe.ToString());
 
+                listView_DG.Items.Add(item);
+            }
         }
     }
 }
