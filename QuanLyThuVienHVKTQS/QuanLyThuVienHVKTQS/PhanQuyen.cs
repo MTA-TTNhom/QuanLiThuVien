8c3883e3138dccad226b9466748db3070313e6f3
tronglong1998 <54692940+tronglong1998@users.noreply.github.com>
2019-09-23 21:18:51 +0700

    Them phan quyen di

--- QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/PhanQuyen.cs
+++ QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/PhanQuyen.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using QuanLyThuVienHVKTQS.Model;
-using QuanLyThuVienHVKTQS.controller;
 namespace QuanLyThuVienHVKTQS
 {
     class PhanQuyen
     {
-
+        public static int quyen = -1;
 
     }
 
