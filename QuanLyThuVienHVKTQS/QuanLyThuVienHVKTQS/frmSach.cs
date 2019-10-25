86fb915708f67a415536c4b5b7a783b227485080
Khoabdattt52 <phambangbang1235@gmail.com>
2019-10-25 09:00:21 +0700

    update frmSach.cs

--- QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/frmSach.cs
+++ QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/frmSach.cs
@@ -119,7 +119,19 @@ namespace QuanLyThuVienHVKTQS
             txtTheLoai.Text = "";
             this.Them_bool = true;
         }
-     
+        public void btn_edit(bool t)
+        {
+            grpEdit.Enabled = t;
+            btnLuuSach.Enabled = t;
+            btnBoQua.Enabled = t;
+            btnThemSach.Enabled = !t;
+            btnSuaSach.Enabled = !t;
+            btnXoaSach.Enabled = !t;
+            btnThoat.Enabled = !t;
+            Them_bool = false;
+            Sua_bool = false;
+  
+        }
         private void btnThoat_Click(object sender, EventArgs e)
         {
             this.Close();
@@ -202,7 +214,49 @@ namespace QuanLyThuVienHVKTQS
                 }
             }
         }
+        private void btnXoaSach_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("ban co thuc su muon xoa?", "Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+            if (result == DialogResult.Yes)
+            {
+                int masach = Convert.ToInt32(txtMaSach.Text);
+                var s = new SachController();
+                if (s.Del(masach))
+                    HienThiSach();
+                else
+                {
+                    MessageBox.Show("khong xoa duoc!");
+                }
+            }
+        }
+        private void btnThemSach_Click(object sender, EventArgs e)
+        {
 
+            btn_edit(true);
+            txtMaSach.Text = "";
+            txtTenSach.Text = "";
+            txtTacGia.Text = "";
+            //txtNXB.Text = "";
+            txtNamXB.Text = "";
+            txtSoTrang.Text = "";
+            txtGiaTien.Text = "";
+            txtSoLuong.Text = "";
+            txtNgonNgu.Text = "";
+            txtTheLoai.Text = "";
+            this.Them_bool = true;
+        }
+        public void btn_edit(bool t)
+        {
+            grpEdit.Enabled = t;
+            btnLuuSach.Enabled = t;
+            btnBoQua.Enabled = t;
+            btnThemSach.Enabled = !t;
+            btnSuaSach.Enabled = !t;
+            btnXoaSach.Enabled = !t;
+            btnThoat.Enabled = !t;
+            Them_bool = false;
+            Sua_bool = false;
+        }
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             int i = 1;
@@ -223,5 +277,14 @@ namespace QuanLyThuVienHVKTQS
             }
         }
 
+        private void label10_Click(object sender, EventArgs e)
+        {
 
+        }
+
+        private void FrmSach_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
