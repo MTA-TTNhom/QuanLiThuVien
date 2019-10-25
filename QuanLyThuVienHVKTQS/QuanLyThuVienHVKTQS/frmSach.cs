b5cd2c1f035e9ddcbd8e8bb8f028deef633ef584
Khoabdattt52 <phambangbang1235@gmail.com>
2019-10-25 09:46:03 +0700

    update sach.cs

--- QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/frmSach.cs
+++ QuanLyThuVienHVKTQS/QuanLyThuVienHVKTQS/frmSach.cs
@@ -33,7 +33,7 @@ namespace QuanLyThuVienHVKTQS
             var s = new SachController();
             l = s.Detail();
             lvSach.Items.Clear();
-            
+
             var i = 1;
 
             var nxb = new NhaXuatBanController();
@@ -98,18 +98,9 @@ namespace QuanLyThuVienHVKTQS
             txtSoLuong.Text = l[index].soluong.ToString();
             txtNgonNgu.Text = l[index].ngonngu.ToString();
             txtTheLoai.Text = l[index].theloai.ToString();
-            txtNamXB.Text = l[index].namxb.ToString();
-
 
         }
-        List<sach> l = null;
-        private bool Them_bool = false;
-        private bool Sua_bool = false;
-        public frmSach()
-        {
-            InitializeComponent();
-            HienThiSach();
-        }
+
         private void btnThemSach_Click(object sender, EventArgs e)
         {
 
@@ -137,7 +128,6 @@ namespace QuanLyThuVienHVKTQS
             btnThoat.Enabled = !t;
             Them_bool = false;
             Sua_bool = false;
-  
         }
         private void btnThoat_Click(object sender, EventArgs e)
         {
@@ -174,9 +164,9 @@ namespace QuanLyThuVienHVKTQS
                 entity.ngonngu = txtNgonNgu.Text;
                 entity.theloai = txtTheLoai.Text;
                 var s = new SachController();
-                int t=s.Add(entity);
-                if(t>0)
-                HienThiSach();
+                int t = s.Add(entity);
+                if (t > 0)
+                    HienThiSach();
             }
             if (Them_bool == false && Sua_bool == true)
             {
@@ -208,21 +198,6 @@ namespace QuanLyThuVienHVKTQS
 
         private void btnXoaSach_Click(object sender, EventArgs e)
         {
-            DialogResult result=MessageBox.Show("ban co thuc su muon xoa?", "Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
-            if (result == DialogResult.Yes)
-            {
-                int masach = Convert.ToInt32(txtMaSach.Text);
-                var s = new SachController();
-                if (s.Del(masach))
-                    HienThiSach();
-                else
-                {
-                    MessageBox.Show("khong xoa duoc!");
-                }
-            }
-        }
-        private void btnXoaSach_Click(object sender, EventArgs e)
-        {
             DialogResult result = MessageBox.Show("ban co thuc su muon xoa?", "Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
             if (result == DialogResult.Yes)
             {
@@ -236,34 +211,7 @@ namespace QuanLyThuVienHVKTQS
                 }
             }
         }
-        private void btnThemSach_Click(object sender, EventArgs e)
-        {
 
-            btn_edit(true);
-            txtMaSach.Text = "";
-            txtTenSach.Text = "";
-            txtTacGia.Text = "";
-            //txtNXB.Text = "";
-            txtNamXB.Text = "";
-            txtSoTrang.Text = "";
-            txtGiaTien.Text = "";
-            txtSoLuong.Text = "";
-            txtNgonNgu.Text = "";
-            txtTheLoai.Text = "";
-            this.Them_bool = true;
-        }
-        public void btn_edit(bool t)
-        {
-            grpEdit.Enabled = t;
-            btnLuuSach.Enabled = t;
-            btnBoQua.Enabled = t;
-            btnThemSach.Enabled = !t;
-            btnSuaSach.Enabled = !t;
-            btnXoaSach.Enabled = !t;
-            btnThoat.Enabled = !t;
-            Them_bool = false;
-            Sua_bool = false;
-        }
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             int i = 1;
@@ -294,4 +242,6 @@ namespace QuanLyThuVienHVKTQS
 
         }
     }
+}
+
 }
